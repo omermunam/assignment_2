c5414353a5b52af599a70802f5959557abe24740
omermunam <73094726+omermunam@users.noreply.github.com>
2020-11-05 16:19:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO I AM OMER MUNAM!
+          HELLO I AM OMER MUNAM 2017219!
         </p>
         <a
           className="App-link"
